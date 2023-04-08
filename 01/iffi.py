92bb49ae8b886bf4b943aa368e7921acff889d9d
st6aaaaaa <110686768+st6aaaaaa@users.noreply.github.com>
2023-04-08 21:02:30 +0300

    Update iffi.py
    исправил 1-ый недочет.

--- 01/iffi.py
+++ 01/iffi.py
@@ -7,9 +7,10 @@ class SomeModel:
 def predict_message_mood(message: str, model: SomeModel,
                          bad_thresholds: float = 0.3,
                          good_thresholds: float = 0.8) -> str:
-
-    if model.predict(message) < bad_thresholds:
+    
+    tmp = model.predict(message)
+    if tmp < bad_thresholds:
         return 'неуд'
-    if model.predict(message) > good_thresholds:
+    if tmp > good_thresholds:
         return 'отл'
     return 'норм'
