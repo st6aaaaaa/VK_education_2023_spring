7700ae34d770c9ec780008b5da154c59f9612951
st6aaaaaa <110686768+st6aaaaaa@users.noreply.github.com>
2023-04-09 20:56:39 +0300

    Update parse.py

--- 02/parse.py
+++ 02/parse.py
@@ -1,22 +1,22 @@
-import json
+mport json
 
 
-def handler(field, word, string):
-    return (field, word, string.count(word))
+def handler(field, word):
+    # реализация не важна
+    pass
 
 
-def parse_json(json_str, keyword_callback,
+def parse_json(json_str, keyword_callback=None,
                required_fields=None, keywords=None):
+
     json_dict = json.loads(json_str)
-    array = []
 
-    if required_fields is None or keywords is None:
-        return None
-
-    for field in required_fields:
-        if field in json_dict.keys():
-            for word in keywords:
-                if word in json_dict[field]:
-                    temp = keyword_callback(field, word, json_dict[field])
-                    array.append(temp)
-    return array if array != [] else None
+    if required_fields is None or keywords is None or \
+            keyword_callback is None:
+        pass
+    else:
+        for field in required_fields:
+            if field in json_dict.keys():
+                for word in keywords:
+                    if word in json_dict[field].split():
+                        keyword_callback(field, word)
