8e04f3bd8054b2cde9210b7e1bdb7c05b9460bdc
st6aaaaaa <110686768+st6aaaaaa@users.noreply.github.com>
2023-04-09 22:33:43 +0300

    Update test_iffi.py

--- 01/test_iffi.py
+++ 01/test_iffi.py
@@ -1,4 +1,5 @@
 from unittest.mock import patch
+from unittest import mock
 import unittest
 import iffi
 
@@ -27,6 +28,9 @@ class TestIffi(unittest.TestCase):
                                           self.class_var, 0.7),
                 'норм')
 
+            expected_calls = [mock.call("something text")] * 3
+            self.assertEqual(expected_calls, mk_var.mock_calls)
+
     def test_iffi_with_goodtreshold(self):
         with patch("iffi.SomeModel.predict") as mk_var:
             mk_var.return_value = 0.2
@@ -50,6 +54,9 @@ class TestIffi(unittest.TestCase):
                                           good_thresholds=0.7),
                 'норм')
 
+            expected_calls = [mock.call("дзен пайтона")] * 3
+            self.assertEqual(expected_calls, mk_var.mock_calls)
+
     def test_iffi_without_bounds(self):
         with patch("iffi.SomeModel.predict") as mk_var:
             mk_var.return_value = 0.2
@@ -60,20 +67,33 @@ class TestIffi(unittest.TestCase):
                                           self.class_var),
                 'неуд')
             mk_var.return_value = 0.75
+            temp = "набор 2"
+            self.assertEqual(type(temp), str)
             self.assertEqual(
                 iffi.predict_message_mood(temp,
                                           self.class_var),
                 'норм')
             mk_var.return_value = 0.8
+            temp = "набор 3"
+            self.assertEqual(type(temp), str)
             self.assertEqual(
                 iffi.predict_message_mood(temp,
                                           self.class_var, 0.7),
                 'норм')
             mk_var.return_value = 0.9
+            temp = "набор 4"
+            self.assertEqual(type(temp), str)
             self.assertEqual(
                 iffi.predict_message_mood(temp,
                                           self.class_var, 0.7),
                 'отл')
+
+            expected_calls = [mock.call("набор букв"),
+                              mock.call("набор 2"),
+                              mock.call("набор 3"),
+                              mock.call("набор 4"),
+                              ]
+            self.assertEqual(expected_calls, mk_var.mock_calls)
 
     def test_iffi_with_bounds(self):
         with patch("iffi.SomeModel.predict") as mk_var:
@@ -86,21 +106,33 @@ class TestIffi(unittest.TestCase):
                                           0.2, 0.5),
                 'норм')
             mk_var.return_value = 0.75
+            temp = "vk education"
+            self.assertEqual(type(temp), str)
             self.assertEqual(
                 iffi.predict_message_mood(temp,
                                           self.class_var,
                                           0.3, 0.35),
                 'отл')
             mk_var.return_value = 0.1
+            temp = "набор 3"
+            self.assertEqual(type(temp), str)
             self.assertEqual(
                 iffi.predict_message_mood(temp,
                                           self.class_var,
                                           0.2, 0.7),
                 'неуд')
             mk_var.return_value = 0.9
+            temp = "setter"
+            self.assertEqual(type(temp), str)
             self.assertEqual(
                 iffi.predict_message_mood(temp,
                                           self.class_var,
                                           0.7, 0.9),
                 'норм')
-            
+
+            expected_calls = [mock.call("another text"),
+                              mock.call("vk education"),
+                              mock.call("набор 3"),
+                              mock.call("setter"),
+                              ]
+            self.assertEqual(expected_calls, mk_var.mock_calls)
