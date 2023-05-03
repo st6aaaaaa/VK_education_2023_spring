d44b68ab64fcf59a495385a40834c59bfd1bf140
st6aaaaaa <110686768+st6aaaaaa@users.noreply.github.com>
2023-05-03 04:09:14 +0300

    Update lru_cache.py

--- 05/lru_cache.py
+++ 05/lru_cache.py
@@ -1,32 +1,55 @@
-from collections import deque
+class Node:
+    def __init__(self, key, val):
+        self.key = key
+        self.val = val
+        self.next = None
+        self.prev = None
 
 
 class LRUCache:
 
     def __init__(self, limit=4):
-        self.deq = deque(maxlen=limit)
-        self.diction = dict()
+        self.diction = {}
+        self.cur_size = 0
+        self.max_size = limit
+        self.head = Node('head', 'head')
+        self.tail = Node('tail', 'tail')
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    @staticmethod
+    def removenode(node):
+        node.prev.next = node.next
+        node.next.prev = node.prev
+
+    def movetoend(self, node):
+        self.tail.prev.next = node
+        node.prev = self.tail.prev
+        node.next = self.tail
+        self.tail.prev = node
 
     def get(self, key):
-        list_of_keys = [item[0] for item in self.deq]
-        if key in list_of_keys:
-            index = list_of_keys.index(key)
-            var = self.deq[index]
-            self.deq.remove(var)
-            self.deq.append(var)
-        return self.diction.get(key, None)
+        if key not in self.diction.keys():
+            return None
+        node = self.diction[key]
+        self.removenode(node)
+        self.movetoend(node)
+        return node.val
 
-    def set(self, key, value) -> None:
-        list_of_keys = [item[0] for item in self.deq]
-        if key not in list_of_keys:
-            if len(self.deq) == self.deq.maxlen:
-                del self.diction[self.deq[0][0]]
-                self.deq.popleft()
-            self.deq.append([key, value])
-            self.diction[key] = value
+    def set(self, key, new_val):
+        if key in self.diction.keys():
+            node = self.diction[key]
+            node.val = new_val
+            self.removenode(node)
+            self.movetoend(node)
         else:
-            index = list_of_keys.index(key)
-            self.deq.remove(self.deq[index])
-            self.deq.append([key, value])
-            self.diction[key] = value
-            
+            if self.cur_size >= self.max_size:
+                lru_node = self.head.next
+                del self.diction[lru_node.key]
+                self.removenode(lru_node)
+                self.cur_size -= 1
+            new_node = Node(key, new_val)
+            self.diction[key] = new_node
+            self.movetoend(new_node)
+            self.cur_size += 1
+             
